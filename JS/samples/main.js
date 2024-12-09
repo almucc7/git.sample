941bd3744ce7da56806b64feadb0a27414102a6b
almucc <almuccdaw@gmail.com>
2024-12-09 20:49:49 +0100

    Update

--- JS/samples/main.js
+++ JS/samples/main.js
@@ -1,7 +1,10 @@
-import {countWords} from './tools.js';
+import {countWords, createPassword} from './tools.js';
+
 
 
 console.log('Soy main');
 
 console.log(countWords('Soy tu amigo'));
+
+console.log(createPassword());
 
--- JS/samples/tools.js
+++ JS/samples/tools.js
@@ -24,4 +24,29 @@ export function countWords(sentence = '') {
 
 //2. Crea una función que genere una contraseña aleatoria con letras mayúsculas, letras minúsculas y números:
 
+export function createPassword() {
+    
+    const allCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let password = '';
+    //Obtengo un número entre 8 y 23, que será el mínimo y máximo de longitud de mis contraseñas
+    const lengthPassword =  Math.floor(Math.random() * (23 - 8 + 1)) + 8;
+    
+    for (let i = 0; i < lengthPassword; i++) {
+        //Math.floor(): redondea un número hacia abajo al entero más cercano
+        //Math.random(): genera un número decimal aleatorio entre 0 (incluido) y 1 (excluido)
 
+        //Genero números aleatorios que serán el índice de extracción de caracteres de allCharacters:        
+        let index = Math.floor(Math.random() * allCharacters.length);
+
+        let char = allCharacters[index]; //Obtengo el caracter correspondiente  
+        
+        // Con una probabilidad del 50%, convierto el carácter a mayúscula
+        if (Math.random() > 0.5) {
+            char = char.toUpperCase();
+        }
+
+        password += char; //Concateno el caracter seleccionado (mayúscula o minúscula)
+    }
+
+    return password;
+}
