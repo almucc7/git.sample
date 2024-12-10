4802b4edce0db22fda5f16f04bc4e7c5b1ec7620
almucc <almuccdaw@gmail.com>
2024-12-10 19:08:59 +0100

    New folder web and updates

--- JS/samples/main.js
+++ JS/samples/main.js
@@ -1,5 +1,4 @@
-import {countWords, createPassword} from './tools.js';
-
+import {countWords, createPassword, generatePassword} from './tools.js';
 
 
 console.log('Soy main');
@@ -7,4 +6,6 @@ console.log('Soy main');
 console.log(countWords('Soy tu amigo'));
 
 console.log(createPassword());
+
+console.log(generatePassword(13));
 
