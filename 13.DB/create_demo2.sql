b950a65623ae31af231095a00b4c39d18aff660b
almucc <almudena.cantarero@gmail.com>
2025-02-14 13:03:59 +0100

    Create scripts

---
+++ 13.DB/create_demo2.sql
@@ -0,0 +1,18 @@
+-- SHOW DATABASES;
+-- SI LA BASE DE DATOS EXISTE, BÓRRALA:
+DROP DATABASE IF EXISTS DEMO2;
+CREATE DATABASE DEMO2;
+-- PARA ACTIVARLA, QUE ES UN DOBLE CLIC EN ELLA:
+USE DEMO2;
+CREATE TABLE ANIMALS (
+	 id char(18) primary key default (uuid()),
+	 name varchar(255) NOT null unique,
+	 englishName varchar (255) NOT null unique,
+     sciName,
+     diet,
+     lifestyle,
+     location
+     slogan text,
+     created_at timestamp DEFAULT (NOW()),
+     updated_at timestamp DEFAULT (NOW())
+);
